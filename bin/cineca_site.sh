21bda2b9d5f4c8fe2fc978c47ed3c879cb1a75c4
Davide Cesari <dcesari@arpae.it>
2017-02-09 18:03:00 +0100

    new module with cineca-specific functions

---
+++ bin/cineca_site.sh
@@ -0,0 +1,31 @@
+getarki_icbc_cineca() {
+
+    local h hinput timerange ana d2h t2h
+
+    for h in `seq $MODEL_START_SLICE $MODEL_FREQ_SLICE $MODEL_STOP_SLICE`; do
+
+	hinput=$(($h+$MODEL_DELTABD_SLICE))
+#	timerange="timerange:Timedef,${hinput}h,254"
+#	reftime=`getarki_datetime $DATES_SLICE $TIMES_SLICE`
+	weekday=`date -u --date $DATES_SLICE +%A`
+	origdir=$CINECA_ARCHIVE_PRE/$TIMES_SLICE/$weekday/$CINECA_ARCHIVE_POST
+	if [ ! -f "$origdir/$DATES_SLICE$TIMES_SLICE " ]; then
+	    return 1
+	fi
+# will not work with analysis
+	origname=`inputmodel_name $hinput`
+	ln -s $origdir/$origname `inputmodel_name $h`
+
+	if [ "$h" -eq "0" ]; then
+	    ana=`inputmodel_name a`
+	    [ -f "$ana" -o -h "$ana" ] || ln -s `inputmodel_name $h` $ana
+	fi
+    done
+
+}
+
+# start exporting all assignments
+set -a
+check_dep cineca_site cosmo_model
+# stop exporting all assignments
+set +a
