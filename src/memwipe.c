740f58d209fccc49a12c780629730a8dec0bbe38
Davide Cesari <dcesari@arpae.it>
2023-06-12 11:26:34 +0200

    Increase the maximum number of buffers and introduce a test option

--- src/memwipe.c
+++ src/memwipe.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NPTRS 64
+#define NPTRS 512
 #define MSIZE 1024LL*1024LL*1024LL*2LL
 
 /*
@@ -22,12 +22,14 @@ if MemFree < MemTotal/2 + memreq/2:
 alloc(MIN(MemTotal/2 + mr/2, MemAvailable))
 */
 
+char meminfofile[512];
 
 void usage(char *prog) {
-    printf("Usage: %s <memreq>\n", prog);
+    printf("Usage: %s [-t [<meminfo file>]] <memreq>\n", prog);
     printf("where memreq is the memory required by the user application in kB.\n");
     printf("Wipes enough memory in order to guarantee the required amount\n");
     printf("balanced among numa nodes. Two numa nodes are assumed for the moment.\n");
+    printf("With -t option (and an optional /proc/meminfo file path) show only the amount\nof memory that would be freed, without taking any action.\n");
     exit(1);
 }
 
@@ -43,7 +45,7 @@ void procfs_getmem(size_t *memtotal, size_t *memfree, size_t *memavailable) {
   *memfree = -1;
   *memavailable = -1;
 
-  meminfo = fopen("/proc/meminfo", "r");
+  meminfo = fopen(meminfofile, "r");
   if (meminfo) {
     while ((nread = getline(&line, &len, meminfo)) != -1) {
       n = sscanf(line, "%60s %60s %60s", head, val, unit);
@@ -62,10 +64,21 @@ void procfs_getmem(size_t *memtotal, size_t *memfree, size_t *memavailable) {
 void main(int argc, char **argv) {
   size_t memtotal, memfree, memavailable, memreq, tobefreed, margin=1024LL*1024LL*1024LL;
   char *mpt[NPTRS];
-  int i, n, numanodes=2;
+  int i, n, numanodes=2, indarg = 1, testmode = 0;
 
   if (argc < 2) usage(argv[0]);
-  memreq = atoll(argv[1]);
+
+  indarg = 1;
+  strncpy(meminfofile, "/proc/meminfo", sizeof(meminfofile));
+  if (!strcmp(argv[indarg], "-t")) {
+    testmode = 1;
+    indarg++;
+    if (argc > 3) {
+      strncpy(meminfofile, argv[indarg], sizeof(meminfofile));
+      indarg++;
+    }
+  }
+  memreq = atoll(argv[indarg]);
   if (memreq <= 0) usage(argv[0]);
 
   procfs_getmem(&memtotal, &memfree, &memavailable);
@@ -82,9 +95,10 @@ void main(int argc, char **argv) {
   printf("memtotal: %zd, memfree: %zd, memavailable: %zd\n", memtotal, memfree, 
 	memavailable);
   tobefreed = tobefreed > memavailable-margin ? memavailable-margin : tobefreed;
-  printf("wiping: %zd kB\n", tobefreed);
   n = (int)((double)(tobefreed)*1024./(double)(MSIZE));
   n = n > NPTRS ? NPTRS : n;
+  printf("wiping: %zd kB in %d buffers\n", tobefreed, n);
+  if (testmode) exit(0);
 
   printf("allocating %d buffers of %zd bytes:\n|", n, (size_t)(MSIZE));
   for (i=0; i<n; i++) {
